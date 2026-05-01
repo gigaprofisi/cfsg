61cb3942427ba5fb0c266c489b4fa53c1883d4c6
gigaprofisi <126360441+gigaprofisi@users.noreply.github.com>
2026-05-01 12:43:01 -0700

    Create BalanceTheorem.lean

---
+++ Gor83/BalanceTheorem.lean
@@ -0,0 +1,23 @@
+-- As described in Gor83, The Classification of Finite Simple Groups, Volume 1: Groups of Noncharacteristic 2 Type, Daniel Gorenstein
+
+
+/-
+
+X: Grp
+
+Theorem [ (
+  (O(X) = 1) AND (Syl2 X is connected)
+) -> (
+  (X is of characteristic 2 type) OR (X is of component type)
+) ]
+
+
+equiv.
+
+Theorem [
+  (O(X) = 1)
+-> (
+  (X is of characteristic 2 type) OR (X is of component type) OR NOT (Syl2 X is connected)
+) ]
+
+-/
