36d95696cc920e1028e47bcd452d40660c3508ad
gigaprofisi <126360441+gigaprofisi@users.noreply.github.com>
2026-04-30 20:03:41 -0700

    Create Iso4.lean

---
+++ Elementary/Iso4.lean
@@ -0,0 +1,15 @@
+-- "Zassenhaus' butterfly lemma"
+-- "fourth isomorphism theorem"
+
+/-
+
+( (Y sube G) and (X nsub Y) and (B sube G) and (A nsub B) ) -> (
+((Y int B)X)/((Y int A)X)
+iso
+(Y int B)/((Y int A)(X int B))
+iso
+((Y int B)A)/((X int B)A)
+)
+
+
+-/
