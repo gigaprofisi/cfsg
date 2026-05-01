6470fa294b1116c11b9be075233db4b323348275
gigaprofisi <126360441+gigaprofisi@users.noreply.github.com>
2026-04-30 19:13:57 -0700

    Create Section3.lean

---
+++ UniquenessCase/Asc83Unq/Section3.lean
@@ -0,0 +1,18 @@
+/-
+
+
+Let p be an odd prime.
+Let P be a p-group of rank at least 3
+Let R,Q,S be normal subgroups of P such that
+  R = Q inters S
+  P/S is cyclic
+
+Theorem1:
+  (X subgrouporequal P and R subgrouporequal X and P = QX) -> (m(X) > 1).
+
+
+Theorem2:
+  Let L elem K such that m3(L) = 1
+  (L iso L2(q) or L iso L3^-pi(Q) or L iso J1) with 0 ne pi == q (mod 3)
+
+-/
