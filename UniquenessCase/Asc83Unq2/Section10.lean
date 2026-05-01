c1d5188db0189d4fe796c102fa1fda0554daea53
gigaprofisi <126360441+gigaprofisi@users.noreply.github.com>
2026-04-30 18:09:50 -0700

    Create Section10.lean

---
+++ UniquenessCase/Asc83Unq2/Section10.lean
@@ -0,0 +1,19 @@
+/-
+
+PRELIMINARIES:
+
+Let G be a group
+Let T be a Sylow-2 subgroup of G
+Let B be an abelian subgroup of G such that
+  B is of odd order
+  BT = TB
+  forall (p elem pi(B)), m_p(B) <= 2
+
+-/
+
+/-
+
+Lem1
+  F*(G) is an alternating group -> B is a 3-group
+
+-/
